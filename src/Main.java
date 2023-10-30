f9a1de17988ecd1f05f19d765471492a98784b9e
laluwisnu <laluwisnu1708@gmail.com>
2023-10-30 11:42:20 +0700

    added a simple program

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Mendefinisikan dua bilangan yang akan dijumlahkan
+        int a = 5;
+        int b = 7;
+
+        // Melakukan penjumlahan
+        int sum = a + b;
+
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil penjumlahan a + b = " + sum);
+    }
+}
